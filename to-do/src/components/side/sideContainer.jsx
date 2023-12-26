4fdd108b7ba53dd93cf311fda315d5fc17a41834
Anna <anna.pak.vl@gmail.com>
2023-12-26 14:08:56 +0600

    reducer files added

--- to-do/src/components/side/sideContainer.jsx
+++ to-do/src/components/side/sideContainer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Side from "./side";
 import { connect } from "react-redux";
 const mapStateToProps = (state) => {};
+const mapDispatchToProps = (dispatch) => {};
 
-const SideContainer = connect(mapStateToProps)(Side);
+const SideContainer = connect(mapStateToProps, mapDispatchToProps)(Side);
 
 export default SideContainer;
