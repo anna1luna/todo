f58e09ca10df227ea62da98aad1b75bd17ef0072
Anna <anna.pak.vl@gmail.com>
2023-12-26 18:21:07 +0600

    design changes to todo

--- to-do/src/components/todo/todo.jsx
+++ to-do/src/components/todo/todo.jsx
@@ -15,6 +15,10 @@ const Todo = (props) => {
             id="task"
             placeholder="Введите название задачи"
           ></input>
+          <div className={s.counter}>
+            <p>*Задача должна содержать больше 30 символов</p>
+            <p> 0 </p>
+          </div>
         </div>
         <select
           className={`form-select ${s.selection}`}
@@ -45,23 +49,53 @@ const Todo = (props) => {
           <tr>
             <td>купить тыкву</td>
             <td>пойди в Магнит за тыквой</td>
-            <td>В работе</td>
+            <td>
+              <select
+                className={`form-select ${s.selectionTasks}`}
+                aria-label="Default select"
+              >
+                <option selected>Выберите статус</option>
+                <option value="1">В работе</option>
+                <option value="2">Ожидание</option>
+                <option value="3">Выполнено</option>
+              </select>
+            </td>
           </tr>
           <tr>
             <td>купить яблоки</td>
             <td>пойди в Магнит за яблоками</td>
-            <td>Ожидание</td>
+            <td>
+              <select
+                className={`form-select ${s.selectionTasks}`}
+                aria-label="Default select"
+              >
+                <option selected>Выберите статус</option>
+                <option value="1">В работе</option>
+                <option value="2">Ожидание</option>
+                <option value="3">Выполнено</option>
+              </select>
+            </td>
           </tr>
           <tr>
             <td>купить корм</td>
             <td>пойди в Магнит за кормом для Яры</td>
-            <td>Выполнено</td>
+            <td>
+              <select
+                className={`form-select ${s.selectionTasks}`}
+                aria-label="Default select"
+              >
+                <option selected>Выберите статус</option>
+                <option value="1">В работе</option>
+                <option value="2">Ожидание</option>
+                <option value="3">Выполнено</option>
+              </select>
+            </td>
           </tr>
           <tr>
             <td colspan="2" className={`table-active`}>
               Общее количество задач:
             </td>
-            <td className={`table-active`}>3</td>
+            <td className={`table-active ${s.total}`}>3</td>
           </tr>
         </tbody>
       </table>
