4d826b427b5ff0b3af0a8068b63bb802c97be1f8
Anna <anna.pak.vl@gmail.com>
2023-12-27 16:54:27 +0600

    deleted side

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/header/header";
-import SideContainer from "./components/side/sideContainer";
 import TodoContainer from "./components/todo/todoContainer";
 
 const App = (props) => {
@@ -8,7 +7,6 @@ const App = (props) => {
     <div>
       <Header />
       <div className="body">
-        <SideContainer />
         <TodoContainer />
       </div>
     </div>
--- to-do/src/components/side/sideContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Side from "./side";
-import { connect } from "react-redux";
-const mapStateToProps = (state) => {};
-const mapDispatchToProps = (dispatch) => {};
-
-const SideContainer = connect(mapStateToProps, mapDispatchToProps)(Side);
-
-export default SideContainer;
